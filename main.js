0643cd7c223ab61aefeed2918166f879f3dc85c3
Nattie87 <natalie.huitson@gmail.com>
2016-07-08 10:33:56 +0100

    Main JS
    Main JS for Memory Game

---
+++ main.js
@@ -0,0 +1,9 @@
+		var cardOne = "queen";
+		var cardTwo = "king";
+		var cardThree = "queen";
+		var cardFour = "king";
+
+		console.log("hello");
+
+		if (cardTwo === cardFour) {alert ("You found a match!")}
+			else {alert ("Sorry, try again")}
